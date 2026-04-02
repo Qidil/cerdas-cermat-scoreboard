6ed71f15179b51f08236757dd50a18a2fa01f340
Aqid <aidhilgamevil@gmail.com>
2026-04-02 23:18:56 +0800

    tes electron

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const { app, BrowserWindow } = require('electron')
 
-let mainWindow
+let displayWindow
+let controlWindow
 
-function createWindow () {
-    mainWindow = new BrowserWindow({
+function createWindow() {
+    // papan skor
+    displayWindow = new BrowserWindow({
         width: 1000,
         height: 700,
+        title: 'Cerdas Cermat Scoreboard',
     })
 
-    mainWindow.loadURL('http://localhost:5173')
+    displayWindow.loadURL('http://localhost:5173/display')
+
+    // bagian kontrol
+    controlWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Cerdas Cermat Control',
+    })
+
+    controlWindow.loadURL('http://localhost:5173/control')
 }
 
 app.whenReady().then(createWindow)
